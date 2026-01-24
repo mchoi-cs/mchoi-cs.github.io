3e408de98dd28a6240f35932128117931d9925f2
mchoi-cs <michellevchoi0@gmail.com>
2026-01-24 11:05:10 -0500

    Add smooth scroll to gallery link

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 import "../css/About.css";
 
 export const About = () => {
@@ -11,14 +12,13 @@ export const About = () => {
               Hi, I'm Michelle. I'm a fourth-year Computer Science student at the University of Ottawa,
               graduating in Spring 2027.
               <br></br>  
-              I'm currently on a 12-month work term working at Ford.
-              {/* for embedded systems. I value curiosity, initiative, and clarity, and I enjoy
-              presenting ideas and talking through systems in reviews and demos.  */}
+              I'm currently on a 12-month work term working at Ford for embedded systems. I value curiosity, initiative, and clarity, and I enjoy
+              presenting ideas and talking through systems in reviews and demos. 
               <br></br>
               Outside of work, I do hackathons, powerlift, and I've been learning to draw. You can see some of my art in the{" "}
-              <a href="#gallery-component" className="inline-link">
+              <Link to="gallery-component" smooth duration={500} className="inline-link">
                 gallery
-              </a>.
+              </Link>.
             </p>
 
             <div className="about-drawingWrap">
